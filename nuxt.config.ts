c38987953a97d9c31547c31fb241d9a4f9265ccd
Foxx Greeley <foxx.greeley@marsworks.com>
2023-09-19 16:59:32 -0400

    fix: solved typescript errors in nuxt.config and updated packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-      modules: [
-        '@nuxtjs/tailwindcss',
-        'nuxt-icon',
-        '@nuxtjs/eslint-module',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
+        },
       ],
-      head: {
-        link: [
-          {
-            rel: 'stylesheet',
-            href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
-          },
-        ],
-        meta: [
-          { hid: 'robots', name: 'robots', content: 'noindex' }
-        ],
-      },
-  })
+      meta: [
+        { hid: 'robots', name: 'robots', content: 'noindex' }
+      ],
+    },
+  },
+})
 
